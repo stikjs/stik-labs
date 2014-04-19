8d242d2b5d1adc69057f0dc9209621a1e891a22b
Lukas Alexandre <lukas@codelogic.me>
2014-04-19 20:35:43 +0200

    Updating to 1.1.0

--- stik-labs.js
+++ stik-labs.js
@@ -5,7 +5,7 @@
 //            See https://github.com/stikjs/stik-labs/blob/master/LICENSE
 // ==========================================================================
 
-// Version: 1.0.0 | From: 19-04-2014
+// Version: 1.1.0 | From: 19-04-2014
 
 window.stik.labs.behavior = function behaviorLab( spec ){
   if ( !spec ) { throw "Stik: Behavior Lab needs an environment to run"; }
